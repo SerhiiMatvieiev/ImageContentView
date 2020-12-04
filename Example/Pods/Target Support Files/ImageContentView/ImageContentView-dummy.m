a4b186311a1e262dadf070a1d15960784013989a
Serhii Matvieiev <serhii.matvieiev@gen.tech>
2020-12-04 11:00:29 +0200

    Add files

---
+++ Example/Pods/Target Support Files/ImageContentView/ImageContentView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ImageContentView : NSObject
+@end
+@implementation PodsDummy_ImageContentView
+@end
---
+++ Example/Pods/Target Support Files/Pods-ImageContentView_Example/Pods-ImageContentView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageContentView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ImageContentView_Example
+@end
